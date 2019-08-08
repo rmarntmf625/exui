dbed77bc7dcb579673b05ea0b347c58e5246db8f
user <user@hit.ac.kr>
2019-08-08 18:22:27 +0900

    0808

---
+++ myplugin.js
@@ -0,0 +1,8 @@
+$.fn.backYellow =  function(){
+    this.css('background-color', 'yellow');
+    return this;
+}
+$.fn.bgColor =  function(option){
+    this.css('background-color', option);
+    return this;
+}
